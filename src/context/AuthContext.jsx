b7e7d7978b95bac52efc30aff0f60f9c2392b064
mad201802 <md148@hdm-stuttgart.de>
2023-05-10 00:42:01 +0200

    Added mock functions to the AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,8 @@ import {
   signOut,
   onAuthStateChanged,
   GoogleAuthProvider,
-  signInWithPopup
+  signInWithPopup,
+  signInWithEmailAndPassword
 } from "firebase/auth";
 import { auth, db } from "../firebase";
 
@@ -21,8 +22,58 @@ export const AuthContextProvider = ({ children }) => {
     };
   }, []);
 
-  const signIn = () => {
+  const signInWithGoogle = () => {
     return signInWithPopup(auth, new GoogleAuthProvider())
+  };
+
+  const signInWithEmail = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signUpWithEmail = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const getUserChats = async (userId) => {
+    //TODO: Implement this
+    return ["chat1", "chat2", "chat3"];
+  };
+
+  const getChatInfo = async (chatId) => {
+    //TODO: Implement this
+    return {
+      participants: ["USER_ID", "USER_ID"],
+    }
+  };
+
+  const getChatMessages = async (chatId) => {
+    //TODO: Implement this
+    return [
+      {
+        "from": "USER_ID",
+        "to": "USER_ID",
+        "message": "Hello World!",
+        "timestamp": 1647483103
+      },
+      {
+        "from": "USER_ID",
+        "to": "USER_ID",
+        "message": "Hello World!",
+        "timestamp": 1647483103
+      },
+      {
+        "from": "USER_ID",
+        "to": "USER_ID",
+        "message": "Hello World!",
+        "timestamp": 1647483103
+      },
+      {
+        "from": "USER_ID",
+        "to": "USER_ID",
+        "message": "Hello World!",
+        "timestamp": 1647483103
+      }
+    ]
   };
 
   const logOut = () => {
@@ -30,7 +81,7 @@ export const AuthContextProvider = ({ children }) => {
   };
 
   return (
-    <UserContext.Provider value={{user, setUser, logOut, signIn }}>
+    <UserContext.Provider value={{user, setUser, logOut, signInWithGoogle, signInWithEmail }}>
       {children}
     </UserContext.Provider>
   );
