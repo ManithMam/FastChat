228c71778fead3fa492b3871b327b7ebb62e85b4
Kethella Oliveira <kethella.oliveira@gmail.com>
2023-05-11 15:57:38 +0200

    signUp page

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, TextField } from '@mui/material';
+import { auth } from '../../firebase';
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
-export default SignUp
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      await auth.createUserWithEmailAndPassword(email, password);
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="w-full min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-6xl font-bold m-5">Create an account</h1>
+      <form onSubmit={handleSignUp}>
+        <div className="m-3">
+          <TextField
+            label="Email"
+            variant="outlined"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="m-3">
+          <TextField
+            label="Password"
+            variant="outlined"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="m-3">
+          <Button type="submit" variant="outlined">
+            Sign Up
+          </Button>
+        </div>
+      </form>
+      {error && <p className="text-red-500 m-3">{error}</p>}
+    </div>
+  );
+};
+
+export default SignUp;
