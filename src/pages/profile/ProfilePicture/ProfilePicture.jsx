f41dd6a37d793222d905f7ce5f1719aa73e9ed1a
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-06-09 14:51:22 +0200

    Add image upload functions

--- src/pages/profile/ProfilePicture/ProfilePicture.jsx
+++ src/pages/profile/ProfilePicture/ProfilePicture.jsx
@@ -1,18 +1,47 @@
 import { Avatar } from "@mui/material";
 import LoginInformation from "./LoginInformation";
 import { UserAuth } from "../../../context/AuthContext";
+import { getStorage, ref, uploadBytes } from "firebase/storage"
 
-function ProfilePicture (){        
+async function readFile(){    
 
-    const {user} = UserAuth();       
+    console.log("Hello!")
+
+    const [fileHandle] = await window.showOpenFilePicker();
+
+    const file = await fileHandle.getFile();
+
+    console.log(file);
+
+    return file;
+}  
+
+async function uploadFile(){
+
+    const storage = getStorage();
+
+    const storageRef = ref(storage, 'images')
+
+    const img = readFile();
+
+    uploadBytes(storageRef, img).then((snaphot) => {
+        console.log('Uploaded img to cloud storage.')
+    })
+}
+
+function ProfilePicture (){   
+
+    const {user} = UserAuth();  
 
     return (
         <div>
-            <Avatar  sx={{width: 450, height: 450}}/>
+            <Avatar sx={{width: 450, height: 450}} onClick={() => {uploadFile()}}/>
             <LoginInformation provider={user.providerId}/>
         </div>        
     )
 
 }
+
+ 
 
 export default ProfilePicture;
