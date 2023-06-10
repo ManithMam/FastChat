1ae936e7b35e2716f0d5aa2e91d916c8c190d632
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-06-10 14:53:19 +0200

    Implement automatic profile image rendering after  image

--- src/pages/profile/ProfilePicture/ProfilePicture.jsx
+++ src/pages/profile/ProfilePicture/ProfilePicture.jsx
@@ -1,8 +1,9 @@
 import { Avatar } from "@mui/material";
 import LoginInformation from "./LoginInformation";
 import { UserAuth } from "../../../context/AuthContext";
-import { getStorage, ref, uploadBytes, getDownloadURL, getBytes } from "firebase/storage"
+import { getStorage, ref, uploadBytes, getDownloadURL} from "firebase/storage"
 import { updateProfile } from "firebase/auth";
+import { useState } from "react";
 
 async function readFile(){   
 
@@ -15,7 +16,27 @@ async function readFile(){
     return file;
 }  
 
-async function uploadFile(user){
+async function updateUserProfile(user, setImgUrl){
+
+    const storage = getStorage();
+
+    const username = user.displayName;    
+
+    const avatarRef = await ref(storage, `avatars/${username}/avatar`)
+
+    const imgUrl = await getDownloadURL(avatarRef)   
+
+    updateProfile(user, {
+        photoURL: imgUrl
+    })
+
+    setImgUrl(imgUrl);
+        
+}
+
+async function uploadFile(user, setImgUrl){
+
+    console.log("start...")
 
     const storage = getStorage();
 
@@ -26,44 +47,26 @@ async function uploadFile(user){
     const img = await readFile();
 
     uploadBytes(storageRef, img).then((snaphot) => {
-        console.log('Uploaded img to cloud storage.')
+        console.log('Uploaded img to cloud storage.')                    
+       
     })
+
+    updateUserProfile(user, setImgUrl);  
+   
 }
 
-function updateUserProfile(user){
+function ProfilePicture (){     
 
-    const storage = getStorage();
-
-    const username = user.displayName;    
-
-    const avatarRef = ref(storage, `avatars/${username}/avatar`)
-
-    getDownloadURL(avatarRef)
-        .then((url) => {
-            updateProfile(user, {
-                photoURL: url
-            })
-            console.log(url)
-            console.log('Updated avatar')
-        })   
-        .catch((err) => {
-            console.log(err)
-        })
-}
-
-function ProfilePicture (){   
-
-    const {user} = UserAuth();       
+    const {user} = UserAuth();    
+    
+    const [imgUrl, setImgUrl] = useState(user.photoURL);
     
     return (
         <div>
-            <Avatar sx={{width: 450, height: 450}} onClick={() => {uploadFile(user)} }/>
+            <Avatar src={imgUrl} sx={{width: 450, height: 450}} onClick={() => {uploadFile(user, setImgUrl)} }/>
             <LoginInformation provider={user.providerId}/>
         </div>        
     )
-
-}
-
- 
+} 
 
 export default ProfilePicture;
