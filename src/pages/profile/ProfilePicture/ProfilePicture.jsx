8fb62077d493a4c496dab01eb57e650b9c1ac048
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-06-10 13:55:32 +0200

    Implement user profile update

--- src/pages/profile/ProfilePicture/ProfilePicture.jsx
+++ src/pages/profile/ProfilePicture/ProfilePicture.jsx
@@ -1,11 +1,10 @@
 import { Avatar } from "@mui/material";
 import LoginInformation from "./LoginInformation";
 import { UserAuth } from "../../../context/AuthContext";
-import { getStorage, ref, uploadBytes } from "firebase/storage"
+import { getStorage, ref, uploadBytes, getDownloadURL, getBytes } from "firebase/storage"
+import { updateProfile } from "firebase/auth";
 
-async function readFile(){    
-
-    console.log("Hello!")
+async function readFile(){   
 
     const [fileHandle] = await window.showOpenFilePicker();
 
@@ -22,7 +21,7 @@ async function uploadFile(user){
 
     const username = user.displayName;    
 
-    const storageRef = ref(storage, `avatars/${username}/avatar.jpg`)
+    const storageRef = ref(storage, `avatars/${username}/avatar`)
 
     const img = await readFile();
 
@@ -31,13 +30,34 @@ async function uploadFile(user){
     })
 }
 
+function updateUserProfile(user){
+
+    const storage = getStorage();
+
+    const username = user.displayName;    
+
+    const avatarRef = ref(storage, `avatars/${username}/avatar`)
+
+    getDownloadURL(avatarRef)
+        .then((url) => {
+            updateProfile(user, {
+                photoURL: url
+            })
+            console.log(url)
+            console.log('Updated avatar')
+        })   
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
 function ProfilePicture (){   
 
-    const {user} = UserAuth();  
+    const {user} = UserAuth();       
     
     return (
         <div>
-            <Avatar sx={{width: 450, height: 450}} onClick={() => {uploadFile(user)}}/>
+            <Avatar sx={{width: 450, height: 450}} onClick={() => {uploadFile(user)} }/>
             <LoginInformation provider={user.providerId}/>
         </div>        
     )
