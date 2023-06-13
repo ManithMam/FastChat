6b94b7a045563d67a24409a201da11f4cf4a155f
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-06-13 14:42:05 +0200

    Implement display name change in auth acocunt

--- src/pages/profile/Accountinformation/information/DisplayName.jsx
+++ src/pages/profile/Accountinformation/information/DisplayName.jsx
@@ -1,10 +1,7 @@
-import { ThemeProvider } from "@emotion/react";
 import { UserAuth } from "../../../../context/AuthContext";
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, DialogContentText, TextField } from "@mui/material"
-import { useState } from "react";
-import { red } from "@mui/material/colors";
-
-
+import { useState, useEffect } from "react";
+import { updateProfile } from "firebase/auth";
 
 function DisplayName (){
  
@@ -12,43 +9,77 @@ function DisplayName (){
 
     const [open, setOpen] = useState(false)
 
+    const [newName, setNewName] = useState('')
+
+    const [name, setName] = useState(user.displayName)
+
+    useEffect(() => {
+        if(user?.uid) {
+          setName(user.displayName)
+        }
+      }, [user])
+
     function handleOpen(){
         setOpen(true)
     }
 
     function handleClose(){
         setOpen(false)
-    }
+        setNewName('')
+    }  
+
+    function updateUserDisplayName(){
+
+        if(newName == ''){
+            alert('Please Enter a new name before submiting')
+            return
+        }
+
+        if(newName == user.displayName){
+            alert('Please Enter a new name before submiting')
+            return
+        }
+
+        console.log(newName)   
+        updateProfile(user, {
+            displayName: newName
+        })             
+        setName(newName)   
+
+        setOpen(false)
+    }   
 
     return (        
         <div className=" flex flex-row mb-20">
             <div className=" flex flex-grow flex-col">
                 <h1 className=" text-slate-100 font-bold text-2xl">Display Name</h1>
-                <p className=" text-slate-100">{user.displayName}</p>                    
+                <p className=" text-slate-100">{name}</p>                    
             </div>    
         <div className=" flex items-center">
-            <Button variant="contained" color="button" size="large" onClick={handleOpen}>Change</Button>   
+            <Button variant="contained" color="button" size="large" onClick={handleOpen}>Change</Button>    
 
-           
             <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Change Display Name</DialogTitle>
                 <DialogContent>
                     <DialogContentText>
                         Enter new display name.
                     </DialogContentText>
-                    <TextField
+                    <form>  
+                        <TextField
                         autoFocus
                         margine="dense"
-                        id="name"
-                        label="New Display Name"
+                        id="name"                       
                         fullWidth
                         variant="standard"
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="button" variant="contained">Cancel</Button>
-                    <Button color="button" variant="contained">Submit</Button>                    
-                </DialogActions>
-            </Dialog>       
+                        onChange={(event) => {setNewName(event.target.value)}}                                   
+                        />
+                        <DialogActions>
+                        <Button onClick={handleClose} color="button" variant="contained" type="button">Cancel</Button>
+                        <Button onClick={updateUserDisplayName} color="button" variant="contained" type="button">Submit</Button>                    
+                        </DialogActions>
+                    </form>                    
+                </DialogContent>                
+            </Dialog>
 
         </div>                  
     </div>         
