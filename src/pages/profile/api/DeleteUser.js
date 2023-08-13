8dcd9c6e7588ba08c1673d76f64356a56e3fd309
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-08-13 11:24:36 +0200

    Implement deletion of profile pic

--- src/pages/profile/api/DeleteUser.js
+++ src/pages/profile/api/DeleteUser.js
@@ -1,31 +1,82 @@
 import { getAuth, deleteUser } from "firebase/auth"
-import { remove, getDatabase, ref } from "firebase/database";
+import { remove, getDatabase, ref, get, child } from "firebase/database";
+import { getStorage, deleteObject } from "firebase/storage";
+import { ref as storageRef} from "firebase/storage";
 
-const deleteUserDB = () => {
-
-    const auth = getAuth();
-    const user = auth.currentUser;
-
-    const db  = getDatabase();   
-
-    remove(ref(db, 'users/' + user.uid))
-}
-
-const deleteUserAuth = () => {
+const deleteProfilePicture = () => {    
 
     const auth = getAuth();
-    const user = auth.currentUser;
+    const user = auth.currentUser;    
 
-    try{
-        deleteUser(user)
+    const uid = user.uid;
+
+    const storage = getStorage();
+
+    const profilePicRef = storageRef(storage, `avatars/${uid}/avatar`)
+
+    try{       
+        deleteObject(profilePicRef)
     }
     catch(err){
         throw err;
     }
 }
 
+const ProfilePicCheck =  async () => {
 
-export const deleteAccount = () => {     
+    const db = getDatabase();
+
+    const auth = getAuth();
+    const user = auth.currentUser;    
+
+    const uid = user.uid;    
+
+    const dbRef = ref(db)
+    
+    await get(child(dbRef, 'users/' + uid + '/profile_picture')).then((snapshot) => {
+        console.log(snapshot.val())
+        if(snapshot.val() == ""){
+            console.log("picture doesnt exist...")
+            return
+        }
+        else{
+            console.log("Picture exists...delete...")
+            deleteProfilePicture();
+        }
+        
+    })
+    .catch((err) => {
+        console.error(err)
+        console.log("error")
+    })      
+
+}
+
+const deleteUserDB = () => {    
+
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    const db  = getDatabase();   
+
+    remove(ref(db, 'users/' + user.uid)) 
+}
+
+const deleteUserAuth = () => {    
+
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    try{
+        deleteUser(user)       
+    }
+    catch(err){
+        throw err;
+    }
+}
+
+export const deleteAccount = async () => {      
+    await ProfilePicCheck();  
     deleteUserDB();    
     deleteUserAuth();      
 }
