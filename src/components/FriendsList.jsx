c1bec4c66751d1955817935215aeec3ab6f6f3ec
Bogdanov Georg <gb037@hdm-stuttgart.de>
2023-08-16 13:57:36 +0200

    Fix too many re-renders error when switching to friends list view

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -4,18 +4,21 @@ import Divider from "@mui/material/Divider";
 import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from "@mui/material"
 
 const AddFriendDialog = () => {
+    
     const [open, setOpen] = useState(false);
 
     const handleAddFriend = () => { setOpen(false) };
+    const handleCloseDialog = () => { setOpen(false) }; // Ich muss das Schließen des Dialogs in dieser Funktion durchführen, weil React ansonsten einen too many re-renders Fehler wirft wenn ich setOpen direkt in der property aufrufe
 
     return (<>
-        <Button variant="outlined" sx={{ margin: "5px" }} onClick={() => { setOpen(true) }}>Add friend</Button>
-        <Dialog open={open} onClose={setOpen(false)}>
+        <Button variant="outlined" sx={{ margin: "5px" }} onClick={() => {setOpen(true)}}>Add friend</Button>
+        <Dialog open={open}>
             <DialogTitle>Add New Friend</DialogTitle>
             <DialogContent>
                 <DialogContentText></DialogContentText>
             </DialogContent>
             <DialogActions>
+                <Button onClick={handleCloseDialog}>Cancel</Button>
                 <Button autoFocus onClick={handleAddFriend}>Add friend</Button>
             </DialogActions>
         </Dialog>
