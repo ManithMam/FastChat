1471944a47c26661c1981e9d1e910eef3740858f
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-08-19 19:46:23 +0200

    Update LoginInformation.tsx

--- src/pages/profile/components/ProfilePicture/LoginInformation.tsx
+++ src/pages/profile/components/ProfilePicture/LoginInformation.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
 
-function LoginInformation({uid}) {
+interface userId {
+    uid: string | undefined;
+}
+
+function LoginInformation({uid}: userId) {
     return(
         <p className=" text-slate-100 flex justify-center mt-3">ID: {uid}</p>
     )
