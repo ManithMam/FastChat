ffc378b0d05839569bc6eea2c8f5c054e7ca12f6
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-08-20 13:46:34 +0200

    Add first working test

--- src/tests/account.test.ts
+++ src/tests/account.test.ts
@@ -1,5 +1,5 @@
 import { UserAuth } from "../context/AuthContext";
-import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getAuth, connectAuthEmulator, deleteUser } from "firebase/auth";
 import { getDatabase, connectDatabaseEmulator, ref, get, update, child, onValue, push, set } from "firebase/database";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { createUser } from "../_api/UserApi";
@@ -12,12 +12,34 @@ beforeEach(() => {
 
     const db = getDatabase();
   // Point to the RTDB emulator running on localhost.
-  connectDatabaseEmulator(db, "127.0.0.1", 9001);
+  connectDatabaseEmulator(db, "127.0.0.1", 9000);
 
 })
 
+
 test('Add user in realtime db', async () => {
-    const sth = signUpWithEmail("usernametest", "displaynametest", "test@mail.com", "myPassword123!")
-    console.log(sth)  
-   
+    const sth = await signUpWithEmail("usernametest", "displaynametest", "testlululu@mail.com", "myPassword123!")
+    expect(sth.success).toBe(true)   
+})
+
+const deleteUserAuth = () => {    
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+  
+  try{
+      if(user != null){
+          deleteUser(user)  
+      }             
+  }
+  catch(err){
+      throw err;
+  }
+}
+
+afterAll(() => {
+  const db = getDatabase();
+  set(ref(db), null);
+  deleteUserAuth();
+  
 })
